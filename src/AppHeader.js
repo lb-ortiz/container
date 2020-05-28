72fba4a97db1fc04c6da3030ffde4dca479484f5
Leonardo Torres <2212.leonardo@gmail.com>
2020-05-28 18:00:00 -0300

    Implementações.

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -4,16 +4,20 @@ import './AppHeader.css';
 
 const AppHeader = () => (
   <header>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@900&display=swap" rel="stylesheet"></link>
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Chettan+2:wght@600&display=swap" rel="stylesheet"></link>
     <div className="center-column">
-      <h1>🍽 Feed me</h1>
+    {/* <Image source={require('../images/icon.png')} /> */}
+    <img className= "img-header" src={require('./images/icon.png')}/>
+      <h1 className = "text-header"> É FEIRAAAAA</h1>
     </div>
     <nav>
       <ol className="center-column">
         <li>
-          <NavLink to="/">Browse restaurants</NavLink>
+          <NavLink to="/">Buscar Lojas</NavLink>
         </li>
         <li>
-          <NavLink to="/random">Surprise me</NavLink>
+          <NavLink to="/random">Estou com Sorte!</NavLink>
         </li>
         <li>
           <NavLink to="/about">About</NavLink>
