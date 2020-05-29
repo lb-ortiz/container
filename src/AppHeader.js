61c80ca754ba324ce19f17f6baea30dd691e6fe8
l_benetti <lucas_benetti@outlook.com>
2020-05-29 01:47:16 -0300

    css edit

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -6,20 +6,21 @@ const AppHeader = () => (
   <header>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@900&display=swap" rel="stylesheet"></link>
     <link href="https://fonts.googleapis.com/css2?family=Baloo+Chettan+2:wght@600&display=swap" rel="stylesheet"></link>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>
     <div className="center-column">
     {/* <Image source={require('../images/icon.png')} /> */}
-    <img className= "img-header" src={require('./images/icon.png')}/>
-      <h1 className = "text-header"> É FEIRAAAAA</h1>
+    <NavLink to="/"><img className= "img-header" src={require('./images/icon.png') }/></NavLink>
+      <h1 className = "text-header"> Feira Demo</h1>
     </div>
     <nav>
       <ol className="center-column">
-        <li>
+        <li href="#" class="w3-bar-item w3-button w3-hover-green" >
           <NavLink to="/">Buscar Lojas</NavLink>
         </li>
-        <li>
+        <li href="#" class="w3-bar-item w3-button w3-hover-green">
           <NavLink to="/random">Estou com Sorte!</NavLink>
         </li>
-        <li>
+        <li href="#" class="w3-bar-item w3-button w3-hover-green">
           <NavLink to="/about">Sobre</NavLink>
         </li>
       </ol>
