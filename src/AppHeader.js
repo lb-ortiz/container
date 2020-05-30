12b5f538cc1097dd165edd346d71ac99169c813f
Leonardo Torres <2212.leonardo@gmail.com>
2020-05-30 01:06:40 -0300

    Ajuste dos hiperlinks e dos espaçamentos do header.

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -6,22 +6,21 @@ const AppHeader = () => (
   <header>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@900&display=swap" rel="stylesheet"></link>
     <link href="https://fonts.googleapis.com/css2?family=Baloo+Chettan+2:wght@600&display=swap" rel="stylesheet"></link>
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>
+
     <div className="center-column">
-    {/* <Image source={require('../images/icon.png')} /> */}
     <NavLink to="/"><img className= "img-header" src={require('./images/icon.png') }/></NavLink>
       <h1 className = "text-header"> Feira Demo</h1>
     </div>
     <nav>
-      <ol className="center-column">
-        <li href="#" class="w3-bar-item w3-button w3-hover-green" >
-          <NavLink to="/">Buscar Lojas</NavLink>
+      <ol className="center-column-2">
+        <li href="#" >
+          <NavLink className="titles" to="/">Buscar Lojas</NavLink>
         </li>
-        <li href="#" class="w3-bar-item w3-button w3-hover-green">
-          <NavLink to="/random">Estou com Sorte!</NavLink>
+        <li href="#">
+          <NavLink className="titles" to="/random">Estou com Sorte!</NavLink>
         </li>
-        <li href="#" class="w3-bar-item w3-button w3-hover-green">
-          <NavLink to="/about">Sobre</NavLink>
+        <li href="#">
+          <NavLink className="titles" to="/about">Sobre</NavLink>
         </li>
       </ol>
     </nav>
